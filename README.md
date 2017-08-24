0fd73aec285c592dddc4b90a77eea8c386a3846d
LitoMore <litomore@gmail.com>
2017-08-24 13:11:50 +0800

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # inquirer-chalk-pipe
-chalk-pipe style input for inquirer.js
+
+chalk-pipe style input for [inquirer.js](https://github.com/SBoudrias/Inquirer.js)
+
+![](https://raw.githubusercontent.com/LitoMore/inquirer-chalk-pipe/master/screenshot.png)
+
+## Install
+
+```bash
+$ npm install inquirer-chalk-pipe
+```
+
+## Usage
+
+```javascript
+inquirer.registerPrompt('chalk-pipe', require('inquirer-chalk-pipe'));
+```
+
+## Example
+
+
+
+## Related
+
+- [inquirer](https://github.com/SBoudrias/Inquirer.js) - A collection of common interactive command line user interfaces
+- [chalk-pipe](https://github.com/LitoMore/chalk-pipe) - Create chalk scheme with a style pipe
+
+## License
+
+MIT © [LitoMore](https://github.com/LitoMore)
---
+++ example.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const inquirer = require('inquirer');
+
+inquirer.registerPrompt('chalk-pipe', require('.'));
+
+const promptList = [
+	{
+		type: 'chalk-pipe',
+		name: 'text',
+		message: 'Text color',
+		default: '#cccccc'
+	}, {
+		type: 'chalk-pipe',
+		name: 'name',
+		message: 'Name color',
+		default: 'green'
+	}, {
+		type: 'chalk-pipe',
+		name: 'at',
+		message: 'ATs color',
+		default: 'cyan'
+	}, {
+		type: 'chalk-pipe',
+		name: 'link',
+		message: 'Link color',
+		default: 'cyan.underline'
+	}, {
+		type: 'chalk-pipe',
+		name: 'tag',
+		message: 'Tag color',
+		default: 'orange.bold'
+	}, {
+		type: 'chalk-pipe',
+		name: 'photo',
+		message: 'Photo color',
+		default: 'gray'
+	}, {
+		type: 'chalk-pipe',
+		name: 'timeago',
+		message: 'Timeago color',
+		default: 'dim.green.italic'
+	}
+];
+
+inquirer.prompt(promptList).then();
